07009fa116e99b20d5bfed7400841ccaf3aaf314
Oliver P <solutionsbyoli@gmail.com>
2025-09-28 20:14:31 -0700

    good starting point next + wxt

---
+++ apps/extension/wxt.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'wxt';
+
+// See https://wxt.dev/api/config.html
+export default defineConfig({
+  modules: ['@wxt-dev/module-react'],
+  runner: {
+    binaries: {
+      chrome: "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe",
+    },
+  },
+});
